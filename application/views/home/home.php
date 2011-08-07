6358f59f82b60201f6fd93a4fcf0132d49ecd149
Canu Fabien <fabien.canu@gmail.com>
2011-08-07 17:14:42 +0200

    * Test de maquette sur nav

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,3 +1,14 @@
-Home du site
+<!doctype html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="index, follow" />
+	<meta name="author" content="Super User" />
+	<title>Accueil</title>
+</head>
+<body>
 
-<a href="/html5">Démos html5</a>
+	<img src="/web/images/html5.jpg" style="display: block; width: 3000px; height: 2000px; position: absolute; top: 50%; margin-top: -1000px; left: 50%; margin-left: -1500px;" />
+</body>
+</html>
